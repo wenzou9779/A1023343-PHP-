45a481fed1d30545f041170252955c04e8a2161b
wenzou9779 <saowalak9779@gmail.com>
2016-06-16 10:26:58 +0800

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,15 @@
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<title>寄封mail吧!</title>
+<body>
+</head>
+<form action="email.php" method="post">
+寄件者:<input type="text" name="A1023343"><br/>
+信件主旨:<input type="text" name="subject"><br/>
+信件內容:<br/><textarea name="mailtext" style="width:400px;height:200px;"></textarea><br/>
+收件者:<input type="text" name="php@nuk.im"><br/>
+<input type="submit" name="確認寄出">
+<input type="reset" name="重新填寫">
+</form>
+</html>
